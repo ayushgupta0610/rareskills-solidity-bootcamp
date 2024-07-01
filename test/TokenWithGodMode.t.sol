819413e81aaa2e9c23551a08e3cafd7567e582b9
Ayush Gupta <7221434+ayushgupta0610@users.noreply.github.com>
2024-07-01 13:06:54 +0530

    Do minor corrections

--- test/TokenWithGodMode.t.sol
+++ test/TokenWithGodMode.t.sol
@@ -2,16 +2,20 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
-import {TokenWithSanctions} from "../src/week1/TokenWithSanctions.sol";
+import {TokenWithGodMode} from "../src/week1/TokenWithGodMode.sol";
 
 contract TokenWithSanctionsTest is Test {
-    TokenWithSanctions public tokenWithSanctions;
+    TokenWithGodMode public tokenWithGodMode;
 
     address public ADMIN = makeAddr("admin");
     string public constant NAME = "TokenWithSanctions";
     string public constant SYMBOL = "TWS";
 
     function setUp() public {
-        tokenWithSanctions = new TokenWithSanctions(ADMIN, NAME, SYMBOL);
+        tokenWithGodMode = new TokenWithGodMode(ADMIN, NAME, SYMBOL);
     }
+
+    // test if the admin/god can transfer tokens between addresses
+    // test if no other person can transfer tokens between addresses
+    // test erc20 functionalities?
 }
